b95df744a865f59705911072c7580983c6293179
ulimy <ulim130@gmail.com>
2022-06-29 11:06:05 +0900

    [BE] chore : 스프링 초기 설정

---
+++ backend/reviewduck/src/main/java/com/reviewduck/ReviewduckApplication.java
@@ -0,0 +1,13 @@
+package com.reviewduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewduckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewduckApplication.class, args);
+	}
+
+}
