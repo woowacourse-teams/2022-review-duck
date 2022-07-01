c81e2e6296ccfa58b59ba4a73037b67f0763007e
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-01 13:54:49 +0900

    [FE] chore : Eslint, Prettier 설치 및 설정
    Co-authored-by: DomMorello <zuzudnf@gmail.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
-import * as ReactDOM from "react-dom/client";
-import App from "./App";
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(<App />);
