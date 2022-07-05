cb4457a5daf2ccbe0d513de77070139c6ec0eb66
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-05 23:10:44 +0900

    [FE] style : Button 컴포넌트 Props Type Export 제거
    Co-authored-by: DomMorello <zuzudnf@gmail.com>

--- frontend/src/core/components/Button/index.tsx
+++ frontend/src/core/components/Button/index.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 import styles from './styles.module.scss';
 import cn from 'classnames';
 
-export interface Props {
+interface Props {
   type: 'button' | 'submit';
   size: 'small' | 'medium' | 'large';
   filled: boolean;
