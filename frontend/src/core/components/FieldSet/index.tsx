8ab9a2de24f084d13cf58a3fd3883c76263cdf26
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-05 23:09:41 +0900

    [FE] feat : FieldSet 컴포넌트 초안 작성
    Co-authored-by: DomMorello <zuzudnf@gmail.com>

---
+++ frontend/src/core/components/FieldSet/index.tsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import { ReactNode } from 'react';
+import cn from 'classnames';
+import Icon from 'core/components/Icon';
+import styles from './styles.module.scss';
+
+interface Props {
+  size: 'large' | 'medium';
+  title: string;
+  description: string;
+  children: ReactNode;
+}
+
+function FieldSet({ title, description, children }: Props) {
+  return (
+    <div className={cn(styles.container)}>
+      <label className={cn(styles.title)}>{title}</label>
+
+      {children}
+
+      <div className={cn(styles.description)}>
+        <Icon code="chevron_right" />
+        <span>{description}</span>
+      </div>
+    </div>
+  );
+}
+
+FieldSet.propTypes = {
+  size: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  children: PropTypes.element.isRequired,
+};
+
+FieldSet.defaultProps = {
+  size: 'medium',
+  description: '',
+};
+
+export default FieldSet;
