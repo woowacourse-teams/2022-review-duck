64d57256a7e6c9002ef82046d1bf6c9633864b43
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-05 12:53:36 +0900

    temp
    Co-authored-by: DomMorello <zuzudnf@gmail.com>

---
+++ frontend/src/core/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import styles from './styles.module.scss';
+
+Button.propTypes = {};
+
+function Button() {
+  return <div className={styles.button}></div>;
+}
+
+export default Button;
