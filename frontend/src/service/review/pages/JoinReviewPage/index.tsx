475c14a49ea70c9bf8b0df9309dbfb37cebf2d04
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-06 17:38:32 +0900

    [FE] feat : 회고 참여 페이지 레이아웃 적용
    Co-authored-by: DomMorello <zuzudnf@gmail.com>

--- frontend/src/service/review/pages/JoinReviewPage/index.tsx
+++ frontend/src/service/review/pages/JoinReviewPage/index.tsx
@@ -4,23 +4,27 @@ import styles from './styles.module.scss';
 import Button from 'common/components/Button';
 import Icon from 'common/components/Icon';
 
+import imageHero from 'assets/images/demo-create.png';
+
 function JoinReviewPage() {
   return (
     <>
-      <div></div>
+      <div className={cn(styles.container, 'flex-container column')}>
+        <img className="" src={imageHero} alt="웰컴 메시지" />
+      </div>
       <div className={cn(styles.container, 'flex-container column')}>
         <FieldSet title="참여 코드" description="회고 참여를 위한 코드를 입력해주세요.">
-          <input type="text" />
+          <input type="text" placeholder="영문과 숫자로 이루어진 코드 8자리를 입력해주세요." />
         </FieldSet>
 
         <div className={cn('button-container horizontal')}>
-          <Button type="submit" size="medium">
-            <Icon code="ads_click"></Icon>
-            참여하기
-          </Button>
           <Button size="medium" outlined>
             <Icon code="cancel"></Icon>
             취소하기
+          </Button>
+          <Button type="submit" size="medium">
+            <Icon code="ads_click"></Icon>
+            참여하기
           </Button>
         </div>
       </div>
