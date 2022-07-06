5a75b2ad39f44b0e924363558b45334a747d3fa6
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-06 13:28:08 +0900

    [FE] config : 개발용 서버 실행 명령어 수정 및 빌드 파일 경로 수정

--- frontend/.webpack/webpack.common.js
+++ frontend/.webpack/webpack.common.js
@@ -51,7 +51,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.join(__dirname, '/build'),
+    path: path.join(__dirname, '../build'),
     filename: 'bundle.js',
   },
   plugins: [
