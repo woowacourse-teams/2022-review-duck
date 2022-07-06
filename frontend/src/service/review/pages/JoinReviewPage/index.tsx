5e82609d5282b2f12099344635761d924719f644
DomMorello <zuzudnf@gmail.com>
2022-07-06 11:39:20 +0900

    [FE] feat : JoinReviewPage 컴포넌트 작성

---
+++ frontend/src/service/review/pages/JoinReviewPage/index.tsx
@@ -0,0 +1,28 @@
+import FieldSet from 'core/components/FieldSet';
+import cn from 'classnames';
+import styles from './styles.module.scss';
+import Button from 'core/components/Button';
+import Icon from 'core/components/Icon';
+
+function JoinReviewPage() {
+  return (
+    <div className={cn(styles.container, 'flex-container column')}>
+      <FieldSet title="참여 코드" description="회고 참여를 위한 코드를 입력해주세요.">
+        <input type="text" />
+      </FieldSet>
+
+      <div className={cn('button-container horizontal')}>
+        <Button type="submit" size="medium">
+          <Icon code="ads_click"></Icon>
+          참여하기
+        </Button>
+        <Button size="medium" outlined>
+          <Icon code="cancel"></Icon>
+          취소하기
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default JoinReviewPage;
