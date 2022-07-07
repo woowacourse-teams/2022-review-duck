812f2172c153eb4c4909bd960f0e6f02eb1c72d9
DomMorello <zuzudnf@gmail.com>
2022-07-07 11:11:11 +0900

    [FE] design : SubmitReviewPage 디자인 수정

--- frontend/src/service/review/pages/SubmitReviewPage/index.tsx
+++ frontend/src/service/review/pages/SubmitReviewPage/index.tsx
@@ -12,7 +12,7 @@ function SubmitReviewPage() {
         <h1 className={cn(styles.title)}>오늘의 기분은 어떤가요?</h1>
         <span className={cn(styles.description)}>체크인 점수를 선택해주세요.</span>
         <ProgressBar percent={6 / 1} />
-        <span className={cn(styles.progress)}>총 6개의 질문 중 1개 답변됨</span>
+        <span className={cn(styles.progressText)}>총 6개의 질문 중 1개 답변됨</span>
       </div>
       <div className={cn(styles.container)}>
         <FieldSet
