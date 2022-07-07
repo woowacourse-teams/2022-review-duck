daf075ffb7716a6be5f721cacfa8b56731d6f050
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-07 13:11:30 +0900

    [FE] feat : Text 컴포넌트 추가

---
+++ frontend/src/common/components/Text/index.tsx
@@ -0,0 +1,30 @@
+import cn from 'classnames';
+import PropTypes from 'prop-types';
+import styles from './styles.module.scss';
+
+const sizeProps = [12, 14, 16, 18, 20, 24, 32, 40, 48] as const;
+
+// TODO: as
+interface Props {
+  className?: string;
+  size: typeof sizeProps[number];
+  weight: 'lighter' | 'normal' | 'bold';
+  children: React.ReactNode;
+}
+
+function Text({ className, size, weight, children }: Props) {
+  return (
+    <p className={cn(className, styles[`size-${size}`], styles[`weight-${weight}`])}>{children}</p>
+  );
+}
+
+Text.PropTypes = {
+  size: PropTypes.oneOf(sizeProps),
+};
+
+Text.defaultProps = {
+  size: 14,
+  weight: 'normal',
+};
+
+export default Text;
