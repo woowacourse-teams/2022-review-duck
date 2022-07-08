8b75e679874273536492222ed1e54d9fdd9ea8d2
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-08 13:06:48 +0900

    [FE] feat : 회고 생성 페이지 기능 완성

--- frontend/src/service/review/pages/CreateReviewFormPage/index.tsx
+++ frontend/src/service/review/pages/CreateReviewFormPage/index.tsx
@@ -3,56 +3,101 @@ import styles from './styles.module.scss';
 import Button from 'common/components/Button';
 import Icon from 'common/components/Icon';
 import QuestionCard from 'service/review/components/QuestionCard';
-import FieldSet from 'common/components/FieldSet';
+import Logo from 'common/components/Logo';
+import TextBox from 'common/components/TextBox';
+import QuestionEditor from 'service/review/components/QuestionEditor';
+import { ChangeEvent, useState } from 'react';
+
+interface Question {
+  questionValue: string;
+}
 
 function CreateReviewFormPage() {
-  // TODO: 로고 넣기, 우측 폼 작성하기
+  const [reviewTitle, setReviewTitle] = useState('');
+  const [questions, setQuestions] = useState<Question[]>([{ questionValue: '' }]);
+
+  const handleUpdateQuestion = (index: number) => (event: ChangeEvent<HTMLInputElement>) => {
+    const copiedQuestions = [...questions];
+    const updatedQuestion = { ...questions[index], questionValue: event.target.value };
+
+    copiedQuestions.splice(index, 1, updatedQuestion);
+    setQuestions(copiedQuestions);
+  };
+
+  const handleAddQuestion = () => {
+    const copiedQuestions = [...questions];
+
+    copiedQuestions.push({ questionValue: '' });
+    setQuestions(copiedQuestions);
+  };
+
+  const handleDeleteQuestion =
+    (index: number) =>
+    ({ currentTarget }: any) => {
+      if (questions.length <= 1) {
+        return;
+      }
+
+      const copiedQuestions = [...questions];
+
+      copiedQuestions.splice(index, 1);
+      setQuestions(copiedQuestions);
+
+      const $beforeTextBox = currentTarget.form.querySelectorAll('input.question')[index - 1];
+
+      $beforeTextBox && $beforeTextBox.focus();
+    };
+
   return (
     <>
       <div className={cn(styles.container, 'flex-container column')}>
-        <QuestionCard
-          numbering={1}
-          type="text"
-          title="이곳에 질문이 표시됩니다."
-          description="이곳에 질문 설명이 표기됩니다."
-        />
+        <Logo />
 
-        <QuestionCard
-          numbering={2}
-          type="text"
-          title="이곳에 질문이 표시됩니다."
-          description="이곳에 질문 설명이 표기됩니다."
-        />
-
-        <QuestionCard
-          numbering={3}
-          type="text"
-          title="이곳에 질문이 표시됩니다."
-          description="이곳에 질문 설명이 표기됩니다."
-        />
+        <div className={cn(styles.previewContainer, 'flex-container column')}>
+          {questions.map(
+            ({ questionValue }, index) =>
+              questionValue && (
+                <QuestionCard
+                  key={index}
+                  numbering={index + 1}
+                  type="text"
+                  title={questionValue}
+                  description="질문 설명이 이곳에 표기됩니다."
+                />
+              ),
+          )}
+        </div>
       </div>
 
-      <div className={cn(styles.container, 'flex-container column')}>
-        <h1 className={cn(styles.title)}>회고 생성하기</h1>
-        <p className={cn(styles.subtitle)}>설명이 입력될 위치입니다.</p>
+      <div>
+        <form className={cn(styles.container, styles.sticky, 'flex-container column')}>
+          <TextBox theme="underline" size="large" placeholder="회고의 제목을 입력해주세요." />
 
-        <FieldSet title="TEST">
-          <div>
-            <input type="text"></input>
-            <Button>+</Button>
+          <div className={cn(styles.itemContainer, 'flex-container column')}>
+            {questions.map(({ questionValue }, index) => (
+              <QuestionEditor
+                key={index}
+                numbering={index + 1}
+                value={questionValue}
+                onChange={handleUpdateQuestion(index)}
+                onAddQuestion={handleAddQuestion}
+                onDeleteQuestion={handleDeleteQuestion(index)}
+              />
+            ))}
           </div>
-        </FieldSet>
 
-        <div className={cn('button-container horizontal')}>
-          <Button size="medium" outlined>
-            <Icon code="cancel"></Icon>
-            취소하기
-          </Button>
-          <Button type="submit" size="medium">
-            <Icon code="drive_file_rename_outline"></Icon>
-            생성하기
-          </Button>
-        </div>
+          <div className={cn('button-container horizontal')}>
+            <Button theme="outlined">
+              <Icon code="cancel" />
+              <span>취소하기</span>
+            </Button>
+
+            <Button type="button">
+              <Icon code="drive_file_rename_outline" />
+              <span>생성하기</span>
+            </Button>
+          </div>
+        </form>
       </div>
     </>
   );
