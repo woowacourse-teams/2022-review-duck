7b0c2669ca3f363f72261327f239051eb740ccee
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-08 13:05:01 +0900

    [FE] feat : TextBox 컴포넌트 이벤트 Prop 추가

--- frontend/src/common/components/TextBox/index.tsx
+++ frontend/src/common/components/TextBox/index.tsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 import styles from './styles.module.scss';
+import { ChangeEvent, FocusEvent, KeyboardEvent } from 'react';
 
 const typeProps = ['default', 'underline'] as const;
 const sizeProps = ['small', 'medium', 'large'] as const;
@@ -10,14 +11,32 @@ interface Props {
   theme: typeof typeProps[number];
   size: typeof sizeProps[number];
   placeholder?: string;
+  value?: string;
+  onFocus?: (event: FocusEvent<HTMLInputElement>) => void;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  onKeyUp?: (event: KeyboardEvent<HTMLInputElement>) => void;
 }
 
-function TextBox({ theme, className, size, placeholder, ...rest }: Props) {
+function TextBox({
+  theme,
+  className,
+  size,
+  placeholder,
+  value,
+  onFocus,
+  onChange,
+  onKeyUp,
+  ...rest
+}: Props) {
   return (
     <input
       type="text"
       className={cn(className, styles.textBox, styles[`theme-${theme}`], styles[`size-${size}`])}
       placeholder={placeholder}
+      value={value}
+      onFocus={onFocus}
+      onChange={onChange}
+      onKeyUp={onKeyUp}
       {...rest}
     />
   );
