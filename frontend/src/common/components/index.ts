2a09ca6d682c024cb410a7e5ab47c7aa8f7289c2
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-12 15:06:00 +0900

    [FE] refactor : 범용 컴포넌트 export 통합

---
+++ frontend/src/common/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from './Button';
+export { default as FieldSet } from './FieldSet';
+export { default as Icon } from './Icon';
+export { default as Logo } from './Logo';
+export { default as ProgressBar } from './ProgressBar';
+export { default as Text } from './Text';
+export { default as TextBox } from './TextBox';
