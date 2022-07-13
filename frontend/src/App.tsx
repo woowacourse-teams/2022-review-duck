4f1a674d0b63d48fd73dbf339d8f8f6bd203055d
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-13 11:36:40 +0900

    [FE] feat : React Query Context Provider 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
+import { ReactNode } from 'react';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
 import PageRoutes from 'PageRoutes';
 
 import 'styles/@app.scss';
 
+const queryClient = new QueryClient();
+
+function ContextRapper({ children }: { children: ReactNode }) {
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
+
 function App() {
-  return <PageRoutes />;
+  return (
+    <ContextRapper>
+      <PageRoutes />
+    </ContextRapper>
+  );
 }
 
 export default App;
