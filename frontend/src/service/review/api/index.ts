79ec85581f7b2c0498e56c933fb4eaf0bb884496
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-16 09:58:27 +0900

    [FE] feat : 회고 도메인 API 정의 추가

--- frontend/src/service/review/api/index.ts
+++ frontend/src/service/review/api/index.ts
@@ -2,20 +2,28 @@ import { Question } from '../types';
 
 import { request } from 'common/utils';
 
-const createForm = (title: string, questions: Partial<Question>[]) =>
-  request('/api/review-forms', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: {
-      reviewTitle: title,
-      questions: questions,
-    },
-  });
+interface CreateFormData {
+  reviewTitle?: string;
+  questions?: Partial<Question>[];
+}
+
+interface UpdateFormData extends CreateFormData {
+  reviewFormCode: string;
+}
+
+const getFormData = async (reviewFormCode: string) =>
+  (await request.get(`/api/review-forms/${reviewFormCode}`)).data;
+
+const createForm = async (query: CreateFormData) =>
+  (await request.post('/api/review-forms', query)).data;
+
+const updateForm = async ({ reviewFormCode, reviewTitle, questions }: UpdateFormData) =>
+  (await request.put(`/api/review-forms/${reviewFormCode}`, { reviewTitle, questions })).data;
 
 const reviewAPI = {
+  getFormData,
   createForm,
+  updateForm,
 };
 
 export default reviewAPI;
