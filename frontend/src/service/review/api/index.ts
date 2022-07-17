55ce494c65ef24695b6348418d3e81fcab860291
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-17 20:44:13 +0900

    [FE] refactor : 타입 선언 정리 및 추가

--- frontend/src/service/review/api/index.ts
+++ frontend/src/service/review/api/index.ts
@@ -1,23 +1,14 @@
-import { Question } from '../types';
+import { ReviewFormRequest } from '../types';
 
 import { request } from 'common/utils';
-
-interface CreateFormData {
-  reviewTitle?: string;
-  questions?: Partial<Question>[];
-}
-
-interface UpdateFormData extends CreateFormData {
-  reviewFormCode: string;
-}
 
 const getFormData = async (reviewFormCode: string) =>
   (await request.get(`/api/review-forms/${reviewFormCode}`)).data;
 
-const createForm = async (query: CreateFormData) =>
+const createForm = async (query: ReviewFormRequest) =>
   (await request.post('/api/review-forms', query)).data;
 
-const updateForm = async ({ reviewFormCode, reviewTitle, questions }: UpdateFormData) =>
+const updateForm = async ({ reviewFormCode, reviewTitle, questions }: ReviewFormRequest) =>
   (await request.put(`/api/review-forms/${reviewFormCode}`, { reviewTitle, questions })).data;
 
 const reviewAPI = {
