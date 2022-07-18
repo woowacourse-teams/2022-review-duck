22e2b52ee83d3850e306c174c26c9e29aa312bbe
ulimy <ulim130@gmail.com>
2022-07-18 15:48:44 +0900

    [BE] config : Version1.sql 순서변경

--- backend/reviewduck/src/main/resources/database/migration/Version1_init.sql
+++ backend/reviewduck/src/main/resources/database/migration/Version1_init.sql
@@ -1,3 +1,11 @@
+create table review_form
+(
+    id           bigint       not null auto_increment,
+    code         varchar(255) not null,
+    review_title varchar(255) not null,
+    primary key (id)
+);
+
 create table answer
 (
     id           bigint not null auto_increment,
@@ -11,8 +19,18 @@ create table question
     question_value varchar(255) not null,
     review_form_id bigint       not null,
     primary key (id),
-    foreign key (review_form_id) references review_form
+    foreign key (review_form_id) references review_form(id)
 );
+
+create table review
+(
+    id             bigint       not null auto_increment,
+    nickname       varchar(255) not null,
+    review_form_id bigint       not null,
+    primary key (id),
+    foreign key (review_form_id) references review_form(id)
+);
+
 
 create table question_answer
 (
@@ -22,25 +40,7 @@ create table question_answer
     review_id              bigint not null,
     question_answers_order integer,
     primary key (id),
-    foreign key (answer_id) references answer,
-    foreign key (question_id) references question,
-    foreign key (review_id) references review
-);
-
-create table review
-(
-    id             bigint       not null auto_increment,
-    nickname       varchar(255) not null,
-    review_form_id bigint       not null,
-    primary key (id),
-    foreign key (review_form_id) references review_form
-);
-
-
-create table review_form
-(
-    id           bigint       not null auto_increment,
-    code         varchar(255) not null,
-    review_title varchar(255) not null,
-    primary key (id)
+    foreign key (answer_id) references answer(id),
+    foreign key (question_id) references question(id),
+    foreign key (review_id) references review(id)
 );
