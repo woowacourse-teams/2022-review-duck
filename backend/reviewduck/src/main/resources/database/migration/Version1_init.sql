a461851e65253a08a0d45aaae071a92115c10ac7
ulimy <ulim130@gmail.com>
2022-07-18 20:05:14 +0900

    [BE] config :  Version1.sql 예외 처리

--- backend/reviewduck/src/main/resources/database/migration/Version1_init.sql
+++ backend/reviewduck/src/main/resources/database/migration/Version1_init.sql
@@ -9,7 +9,7 @@ create table review_form
 create table answer
 (
     id           bigint not null auto_increment,
-    answer_value varchar(255),
+    answer_value varchar(255) not null,
     primary key (id)
 );
 
@@ -17,30 +17,31 @@ create table question
 (
     id             bigint       not null auto_increment,
     question_value varchar(255) not null,
-    review_form_id bigint       not null,
+    review_form_id bigint,
+    position       integer      not null,
     primary key (id),
-    foreign key (review_form_id) references review_form(id)
+    foreign key (review_form_id) references review_form (id)
 );
 
 create table review
 (
     id             bigint       not null auto_increment,
     nickname       varchar(255) not null,
-    review_form_id bigint       not null,
+    review_form_id bigint,
     primary key (id),
-    foreign key (review_form_id) references review_form(id)
+    foreign key (review_form_id) references review_form (id)
 );
 
 
 create table question_answer
 (
-    id                     bigint not null auto_increment,
-    answer_id              bigint not null,
-    question_id            bigint not null,
-    review_id              bigint not null,
-    question_answers_order integer,
+    id          bigint  not null auto_increment,
+    answer_id   bigint,
+    question_id bigint,
+    review_id   bigint,
+    position    integer not null,
     primary key (id),
-    foreign key (answer_id) references answer(id),
-    foreign key (question_id) references question(id),
-    foreign key (review_id) references review(id)
+    foreign key (answer_id) references answer (id),
+    foreign key (question_id) references question (id),
+    foreign key (review_id) references review (id)
 );
