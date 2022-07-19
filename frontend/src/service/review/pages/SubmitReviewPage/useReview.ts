d4c10e409643817581ae454b3d6b559c5a574a71
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-19 16:42:31 +0900

    [FE] refactor : useReview 커스텀훅 사용하지 않는 옵션 제거

--- frontend/src/service/review/pages/SubmitReviewPage/useReview.ts
+++ frontend/src/service/review/pages/SubmitReviewPage/useReview.ts
@@ -5,12 +5,8 @@ import { ReviewForm } from 'service/review/types';
 import reviewAPI from 'service/review/api';
 
 function useReview(reviewFormCode: string) {
-  const getQuestionsQuery = useQuery<any, any, ReviewForm>(
-    ['questions', reviewFormCode],
-    () => reviewAPI.getQuestions(reviewFormCode),
-    {
-      useErrorBoundary: false,
-    },
+  const getQuestionsQuery = useQuery<any, any, ReviewForm>(['questions', reviewFormCode], () =>
+    reviewAPI.getQuestions(reviewFormCode),
   );
 
   const createMutation = useMutation(reviewAPI.submitAnswer);
