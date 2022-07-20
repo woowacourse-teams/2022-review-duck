2c134f43baf81177e1cde13abe8af4f97a4dac02
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-20 22:16:59 +0900

    [FE] feat : QuestionContent 컴포넌트 추가
    Co-authored-by: DomMorello <zuzudnf@gmail.com>

---
+++ frontend/src/service/review/components/QuestionContent/index.stories.jsx
@@ -0,0 +1,20 @@
+import QuestionContent from '.';
+
+export default {
+  title: 'review/components/QuestionContent',
+  component: QuestionContent,
+};
+
+const Template = (args) => <QuestionContent {...args} />;
+
+export const DefaultQuestionContent = Template.bind({});
+
+DefaultQuestionContent.args = {
+  questions: [
+    {
+      questionValue: '지금 기분은 어떠신가요?',
+      questionDescription: '회고 전 체크인 점수를 입력해주세요.',
+      answerValue: '6점, 싫어요.',
+    },
+  ],
+};
