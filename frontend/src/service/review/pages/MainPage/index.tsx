684648c819d5f625c3a433a03e850781c0387e6e
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-23 16:01:12 +0900

    [FE] refactor : 메인 페이지 styles 입력 방식 수정
    Co-authored-by: DomMorello <zuzudnf@gmail.com>

--- frontend/src/service/review/pages/MainPage/index.tsx
+++ frontend/src/service/review/pages/MainPage/index.tsx
@@ -1,7 +1,5 @@
 import { Link } from 'react-router-dom';
 
-import cn from 'classnames';
-
 import Button from 'common/components/Button';
 import Icon from 'common/components/Icon';
 import Text from 'common/components/Text';
@@ -13,18 +11,18 @@ import styles from './styles.module.scss';
 function MainPage() {
   return (
     <>
-      <div className={cn(styles.container)}>
+      <div className={styles.container}>
         <img className="" src={imageHero} alt="웰컴 메시지" />
       </div>
-      <div className={cn(styles.container)}>
-        <Text className={cn(styles.title)} size={32} weight="bold">
+      <div className={styles.container}>
+        <Text className={styles.title} size={32} weight="bold">
           회고를 통해 팀원과 함께 성장하세요!
         </Text>
-        <Text className={cn(styles.subtitle)} size={18} weight="lighter">
+        <Text className={styles.subtitle} size={18} weight="lighter">
           팀원과 공유할 회고 질문을 작성하고 팀원들에게 참여 코드를 공유하면 팀원들의 회고를
           간편하게 모아 볼 수 있습니다
         </Text>
-        <div className={cn('button-container horizontal')}>
+        <div className="button-container horizontal">
           <Link to={'/review-forms'}>
             <Button type="button" filled>
               <Icon type="outlined" code="maps_ugc" />
