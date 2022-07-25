a75dd1c318797c47a89f47f7930d4964e42343a1
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-25 16:01:28 +0900

    [FE] feat : ReviewLayout Suspense 임시 적용

--- frontend/src/service/review/layout/ReviewLayout/index.tsx
+++ frontend/src/service/review/layout/ReviewLayout/index.tsx
@@ -1,13 +1,30 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { Logo } from 'common/components';
+
+import Skeleton from 'common/components/Skeleton';
+
 import styles from './styles.module.scss';
 
 function ReviewLayout() {
   return (
     <div className={styles.background}>
       <div className={styles.container}>
-        <Suspense fallback={<p>TODO: 로딩 페이지 만들기</p>}>
+        <Suspense
+          fallback={
+            <>
+              <div className="flex-container column" style={{ gap: '2rem' }}>
+                <Logo />
+                <Skeleton line={3} />
+              </div>
+
+              <div className="flex-container column" style={{ gap: '2rem' }}>
+                <Skeleton line={4} />
+              </div>
+            </>
+          }
+        >
           <Outlet />
         </Suspense>
       </div>
