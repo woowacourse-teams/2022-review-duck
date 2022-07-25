dd5a1f88e7b0f3d6a5db5178df5d8c4243fe223d
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-26 02:33:02 +0900

    [FE] feat : 디자인 토큰 컨버터 테마 기능 추가

---
+++ frontend/.design-tokens/CssThemeVarConverter.js
@@ -0,0 +1,78 @@
+const CONFIG_THEME_EXCEPTION_PREFIX = ['global', '$themes'];
+
+class CssThemeVarConverter {
+  #sassContent = '';
+  #cssThemeContent = {};
+
+  convert(designToken = {}) {
+    const themeTokens = Object.entries(designToken).reduce((prev, [key, value]) => {
+      const isExceptionKey = CONFIG_THEME_EXCEPTION_PREFIX.includes(key);
+
+      if (isExceptionKey === true) {
+        return prev;
+      }
+
+      const current = { ...prev };
+
+      current[key] = value;
+      return current;
+    }, new Object());
+
+    this.#tokenParser(themeTokens);
+    return this.#getConvertResult();
+  }
+
+  #tokenParser(data, nameStack = []) {
+    const isVariable = !!data.type;
+
+    if (isVariable) {
+      this.#addVariable(nameStack);
+    }
+
+    const isDeep = typeof data === 'object';
+
+    if (isDeep) {
+      for (const key in data) {
+        const currentNameStack = this.#getVariableNameStack(nameStack, key);
+        this.#tokenParser(data[key], currentNameStack);
+      }
+    }
+  }
+
+  #getVariableNameStack(currentNameStack = [], key) {
+    const name = key.toUpperCase();
+
+    if (currentNameStack.length === 0) {
+      const initialNamePrefix = name;
+      return [initialNamePrefix];
+    }
+
+    const updateNameStack = [...currentNameStack];
+    updateNameStack.push(name);
+
+    return updateNameStack;
+  }
+
+  #addVariable(nameStack = []) {
+    const themeType = nameStack[0].toLowerCase();
+    const targetTheme = [...(this.#cssThemeContent[themeType] || [])];
+
+    const variableKey = `$${nameStack.join('_')}`;
+    const themeKey = [...nameStack].splice(1).join('-');
+
+    targetTheme.push(`\t--THEME-${themeKey}: ${variableKey};`);
+
+    this.#cssThemeContent[themeType] = targetTheme;
+    this.#sassContent += `$THEME-${themeKey}: var(--THEME-${themeKey});\n`;
+  }
+
+  #getConvertResult() {
+    const cssThemeVariable = Object.entries(this.#cssThemeContent)
+      .map(([themeKey, variable]) => `:root[data-theme="${themeKey}"] {\n${variable.join('\n')}\n}`)
+      .join('');
+
+    return { cssThemeVariable, sassThemeVariable: this.#sassContent };
+  }
+}
+
+module.exports = CssThemeVarConverter;
