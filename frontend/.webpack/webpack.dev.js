52e61c087017d72425ed4d2ff2f2fdf9950dfd8b
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-30 23:35:31 +0900

    [FE] config : Webpack Devtool 옵션 변경

--- frontend/.webpack/webpack.dev.js
+++ frontend/.webpack/webpack.dev.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = (env, options) =>
   merge(common(env, options), {
     mode: 'development',
-    devtool: 'eval',
+    devtool: 'eval-source-map',
     devServer: {
       historyApiFallback: true,
       port: 3000,
