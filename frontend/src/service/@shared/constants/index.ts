58f13d74212f3e1336cbb7e2c050290d19ddef82
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-07-30 15:48:32 +0900

    [FE] feat : 인증/인가 관련 API 및 Query Hook 추가

--- frontend/src/service/@shared/constants/index.ts
+++ frontend/src/service/@shared/constants/index.ts
@@ -4,11 +4,40 @@ const PAGE_LIST = {
   REVIEW: '/review',
   REVIEW_JOIN: '/review/join',
   REVIEW_OVERVIEW: '/overview',
+  AUTHORIZE: '/authorize',
 };
 
 const QUERY_KEY = {
-  GET_REVIEWS: 'getReviews',
-  GET_REVIEW_FORM: 'getReviewForm',
+  DATA: {
+    USER: 'user',
+
+    REVIEW: 'review',
+    REVIEW_FORM: 'review-form',
+  },
+  API: {
+    GET_ACCESS_TOKEN: 'getRefreshedAccessToken',
+    GET_USER_PROFILE: 'getUserProfile',
+
+    GET_REVIEWS: 'getReviews',
+    GET_REVIEW_FORM: 'getReviewForm',
+  },
 };
 
-export { PAGE_LIST, QUERY_KEY };
+const ACCESS_PERMISSION = {
+  NON_LOGIN: false,
+};
+
+const ACCESS_TOKEN_EXPIRE_TIME = 60 * 10 * 1000;
+
+const ACCESS_TOKEN_REFRESH_TIME = ACCESS_TOKEN_EXPIRE_TIME - 60 * 2 * 1000;
+
+const ACCESS_VALID_TIME = 60;
+
+export {
+  PAGE_LIST,
+  QUERY_KEY,
+  ACCESS_PERMISSION,
+  ACCESS_TOKEN_EXPIRE_TIME,
+  ACCESS_TOKEN_REFRESH_TIME,
+  ACCESS_VALID_TIME,
+};
---
+++ frontend/src/service/@shared/hooks/useAuth.ts
@@ -0,0 +1,33 @@
+import useGetAccessToken from 'service/@shared/hooks/queries/user/useGetAccessToken';
+
+import { axiosInstanceUtils } from '../utils';
+
+import useCreateRefreshToken from './queries/user/useCreateRefreshToken';
+import useGetUserProfile from './queries/user/useGetUserProfie';
+
+function useAuth() {
+  const createRefreshToken = useCreateRefreshToken({
+    onSuccess: ({ accessToken }) => {
+      axiosInstanceUtils.setHeader('Authorization', `Bearer ${accessToken}`);
+    },
+  });
+
+  const getAccessTokenQuery = useGetAccessToken({
+    onSuccess: ({ accessToken }) => {
+      axiosInstanceUtils.setHeader('Authorization', `Bearer ${accessToken}`);
+    },
+    onError: () => {
+      axiosInstanceUtils.removeHeader('Authorization');
+    },
+  });
+
+  const getUserProfileQuery = useGetUserProfile({
+    enabled: false,
+  });
+
+  const isLogin = getAccessTokenQuery.isSuccess; // TODO: getUserProfileQuery
+
+  return { createRefreshToken, getAccessTokenQuery, getUserProfileQuery, isLogin };
+}
+
+export default useAuth;
