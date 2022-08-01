9bba5fb86cf4e300ec8e09ad82d027faa0531e6a
DomMorello <zuzudnf@gmail.com>
2022-08-02 00:07:35 +0900

    [FE] feat : MyPage 내 ReviewList 컴포넌트 작성
    - TODO: 디자인 스타일링 구현

---
+++ frontend/src/service/community/pages/MyPage/containers/ReviewList/index.tsx
@@ -0,0 +1,106 @@
+import { Link, useNavigate } from 'react-router-dom';
+
+import useGetMyReviewForms from 'service/community/hooks/queries/useGetMyReviewForms';
+import useGetMyReviews from 'service/community/hooks/queries/useGetMyReviews';
+import useDeleteReview from 'service/review/hooks/queries/useDeleteReview';
+import useDeleteReviewForm from 'service/review/hooks/queries/useDeleteReviewForm';
+
+import { Text, Icon } from 'common/components';
+
+import QuestionContent from 'service/@shared/components/QuestionContent';
+import Reaction from 'service/review/components/Reaction';
+
+import styles from './styles.module.scss';
+
+import { PAGE_LIST, TAB } from 'service/@shared/constants';
+
+function ReviewList({ filter }: Record<'filter', string>) {
+  const { data: myReviews } = useGetMyReviews({ enabled: filter === TAB.MY_REVIEWS });
+  const { data: myReviewForms } = useGetMyReviewForms({ enabled: filter === TAB.MY_REVIEW_FORMS });
+
+  const deleteReviewMutation = useDeleteReview();
+
+  const deleteReviewFormMutation = useDeleteReviewForm();
+
+  const navigate = useNavigate();
+
+  const onMoveToOverView = (reviewFormCode: string) => {
+    navigate(`${PAGE_LIST.REVIEW_OVERVIEW}/${reviewFormCode}`);
+    // TODO: 디자인 완성 후 선언적으로 해당 엘리먼트에 추가할 예정
+  };
+
+  const onEditReview = (reviewFormCode: string) => {
+    navigate(`${PAGE_LIST.REVIEW}/${reviewFormCode}`);
+    // TODO: 디자인 완성 후 선언적으로 해당 엘리먼트에 추가할 예정
+  };
+
+  const onEditReviewForm = (reviewFormCode: string) => {
+    navigate(`${PAGE_LIST.REVIEW_FORM}/${reviewFormCode}`);
+    // TODO: 디자인 완성 후 선언적으로 해당 엘리먼트에 추가할 예정
+  };
+
+  const onDeleteReview = (reviewId: number) => {
+    deleteReviewMutation.mutate(reviewId);
+    // TODO: 디자인 완성 후 해당 엘리먼트에 이벤트 등록 예정
+  };
+
+  const onDeleteReviewForm = (reviewFormCode: string) => {
+    deleteReviewFormMutation.mutate(reviewFormCode);
+    // TODO: 디자인 완성 후 해당 엘리먼트에 이벤트 등록 예정
+  };
+
+  return (
+    <>
+      <div className={styles.title}>
+        <Text size={20}>{filter === TAB.MY_REVIEWS ? '내가 작성한 회고' : '내가 생성한 회고'}</Text>
+      </div>
+
+      {filter === TAB.MY_REVIEWS
+        ? myReviews?.reviews.map((review) => (
+            <div className={styles.reviewContainer} key={review.reviewId}>
+              <div className={styles.header}>
+                <Text size={24}>{review.reviewForm.title}</Text>
+                <div className={styles.buttonContainer}>
+                  <Icon code="arrow_right_alt"></Icon>
+                  <Icon code="edit"></Icon>
+                  <Icon code="delete"></Icon>
+                </div>
+              </div>
+              <hr />
+
+              <QuestionContent questions={review.answers}></QuestionContent>
+
+              <hr />
+
+              <Reaction />
+            </div>
+          ))
+        : myReviewForms?.reviewForms.map((reviewForm) => (
+            <div className={styles.reviewContainer} key={reviewForm.code}>
+              <div className={styles.header}>
+                <Text size={24}>{reviewForm.title}</Text>
+                <div className={styles.buttonContainer}>
+                  <Icon code="arrow_right_alt"></Icon>
+                  <Link
+                    to={`${PAGE_LIST.REVIEW_FORM}/${reviewForm.code}`}
+                    state={{ redirect: `${PAGE_LIST.MY_PAGE}` }}
+                  >
+                    <Icon code="edit"></Icon>
+                  </Link>
+                  <Icon code="delete"></Icon>
+                </div>
+              </div>
+              <hr />
+
+              <QuestionContent questions={reviewForm.questions}></QuestionContent>
+
+              <hr />
+
+              <Reaction />
+            </div>
+          ))}
+    </>
+  );
+}
+
+export default ReviewList;
