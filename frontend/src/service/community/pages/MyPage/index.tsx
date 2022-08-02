94682fbb5a44178223ecb6b62af3dc1e7cd5ba2b
DomMorello <zuzudnf@gmail.com>
2022-08-02 17:17:06 +0900

    [FE] feat : MyPage User 정보 API 연결 및 세부사항 구현
    - 깃헙 프로필 연결
    - 탭뷰 변경 구현

--- frontend/src/service/community/pages/MyPage/index.tsx
+++ frontend/src/service/community/pages/MyPage/index.tsx
@@ -6,28 +6,16 @@ import { Button, Icon, Text } from 'common/components';
 
 import LayoutContainer from 'service/@shared/components/LayoutContainer';
 
-import imageRuna from 'assets/images/runa.jpg';
-
 import styles from './styles.module.scss';
 
 import ReviewList from './containers/ReviewList';
 import useMyPageQueries from './useMyPageQueries';
 import { MYPAGE_TAB } from 'service/@shared/constants';
 
-const user = {
-  profileUrl: 'hello',
-  githubNickname: 'lunatic',
-  githubStatus: 'world',
-  nickname: '루나',
-  numberOfReviews: 12,
-  numberOfRevieForms: 4,
-};
-
 function MyPage() {
   const [currentTab, setCurrentTab] = useState(MYPAGE_TAB.MY_REVIEWS);
-  /* TODO: 사용자 관련 정보 API 요청 */
 
-  const { isError, error } = useMyPageQueries(currentTab);
+  const { myReviews, myReviewForms, userProfile, isError, error } = useMyPageQueries();
 
   useEffect(() => {
     if (isError) {
@@ -41,21 +29,27 @@ function MyPage() {
 
   return (
     <>
-      <div className={styles.profileBackground} style={{ backgroundImage: `url(${imageRuna})` }} />
+      <div
+        className={styles.profileBackground}
+        style={{ backgroundImage: `url(${userProfile.profileUrl})` }}
+      />
 
       <LayoutContainer className={styles.container}>
         <aside className={styles.sideContent}>
-          <div className={styles.profileImage} style={{ backgroundImage: `url(${imageRuna})` }}>
+          <div
+            className={styles.profileImage}
+            style={{ backgroundImage: `url(${userProfile.profileUrl})` }}
+          >
             <div className={styles.activeIcon}>🦖</div>
           </div>
 
           <div className={styles.nameCard}>
             <Text size={24} weight="bold">
-              Ryu Hyun Seung
+              {userProfile.nickname}
             </Text>
 
             <Text size={14} weight="lighter">
-              compy-ryu
+              {userProfile.socialId}
             </Text>
           </div>
 
@@ -65,10 +59,16 @@ function MyPage() {
               <span>Edit</span>
             </Button>
 
-            <Button size="small" theme="outlined">
-              <Icon code="person" />
-              <span>Github Profile</span>
-            </Button>
+            <a
+              href={`https://github.com/${userProfile.socialId}`}
+              target="_blank"
+              rel=" noopener noreferrer"
+            >
+              <Button size="small" theme="outlined">
+                <Icon code="person" />
+                <span>Github Profile</span>
+              </Button>
+            </a>
           </div>
 
           <hr className={styles.line} />
@@ -78,8 +78,22 @@ function MyPage() {
               회고 목록
             </Text>
 
-            <li className={cn(styles.item, styles.focus)}>작성한 회고글</li>
-            <li className={styles.item}>생성한 회고</li>
+            <li
+              className={cn(styles.item, { [styles.focus]: currentTab === MYPAGE_TAB.MY_REVIEWS })}
+              onClick={onChangeTab(MYPAGE_TAB.MY_REVIEWS)}
+              onKeyDown={onChangeTab(MYPAGE_TAB.MY_REVIEWS)}
+            >
+              작성한 회고글
+            </li>
+            <li
+              className={cn(styles.item, {
+                [styles.focus]: currentTab === MYPAGE_TAB.MY_REVIEW_FORMS,
+              })}
+              onClick={onChangeTab(MYPAGE_TAB.MY_REVIEW_FORMS)}
+              onKeyDown={onChangeTab(MYPAGE_TAB.MY_REVIEW_FORMS)}
+            >
+              생성한 회고
+            </li>
           </ul>
 
           <hr className={styles.line} />
@@ -87,13 +101,13 @@ function MyPage() {
           <div className={styles.counterContainer}>
             <div className={styles.counter}>
               <Text className={styles.number} size={24} weight="bold">
-                32
+                {myReviews.numberOfReviews}
               </Text>
               <Text size={12}>회고 작성</Text>
             </div>
             <div className={styles.counter}>
               <Text className={styles.number} size={24} weight="bold">
-                7
+                {myReviewForms.numberOfReviewForms}
               </Text>
               <Text size={12}>생성</Text>
             </div>
