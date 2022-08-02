4f5c46a61f1816cd4eda352f927b237197043d4e
DomMorello <zuzudnf@gmail.com>
2022-08-02 11:38:57 +0900

    [FE] refactor : 핸들러 네이밍 컨벤션에 맞게 수정

--- frontend/src/service/community/pages/MyPage/index.tsx
+++ frontend/src/service/community/pages/MyPage/index.tsx
@@ -20,21 +20,27 @@ function MyPage() {
   const [currentTab, setCurrentTab] = useState(MYPAGE_TAB.MY_REVIEWS);
   /* TODO: 사용자 관련 정보 API 요청 */
 
+  const onChangeTab = (filter: string) => () => {
+    setCurrentTab(filter);
+  };
+
   return (
     <div className={styles.mypageContainer}>
       <ProfileCard user={user} />
 
       <div className={styles.tabContainer}>
-        <button onClick={() => setCurrentTab(MYPAGE_TAB.MY_REVIEWS)}>작성한 회고 보기</button>
-        <button onClick={() => setCurrentTab(MYPAGE_TAB.MY_REVIEW_FORMS)}>생성한 회고 보기</button>
+        <button onClick={onChangeTab(MYPAGE_TAB.MY_REVIEWS)}>작성한 회고 보기</button>
+        <button onClick={onChangeTab(MYPAGE_TAB.MY_REVIEW_FORMS)}>생성한 회고 보기</button>
       </div>
 
       <section className={styles.reviewListContainer}>
-        {currentTab === MYPAGE_TAB.MY_REVIEWS ? (
-          <ReviewList filter={MYPAGE_TAB.MY_REVIEWS} />
-        ) : (
-          <ReviewList filter={MYPAGE_TAB.MY_REVIEW_FORMS} />
-        )}
+        <ReviewList
+          filter={
+            currentTab === MYPAGE_TAB.MY_REVIEWS
+              ? MYPAGE_TAB.MY_REVIEWS
+              : MYPAGE_TAB.MY_REVIEW_FORMS
+          }
+        />
       </section>
     </div>
   );
