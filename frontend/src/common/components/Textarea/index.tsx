07d2ce88cb24e0aa4c0b3d23194f736c562b1351
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-08-07 22:42:09 +0900

    [FE] refactor : Textarea 컴포넌트 autoResize prop 추가

--- frontend/src/common/components/Textarea/index.tsx
+++ frontend/src/common/components/Textarea/index.tsx
@@ -10,13 +10,14 @@ const propSizeType = ['small', 'medium', 'large'] as const;
 interface Props extends Omit<React.TextareaHTMLAttributes<HTMLTextAreaElement>, 'size'> {
   className?: string;
   size?: typeof propSizeType[number];
+  autoResize?: boolean;
 }
 
-function Textarea({ className, size = 'medium', value, ...rest }: Props) {
+function Textarea({ className, size = 'medium', autoResize, value, ...rest }: Props) {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
   useEffect(() => {
-    if (!textareaRef.current) return;
+    if (!autoResize || !textareaRef.current) return;
 
     textareaRef.current.style.height = '0px';
     textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
@@ -34,6 +35,12 @@ function Textarea({ className, size = 'medium', value, ...rest }: Props) {
 
 Textarea.propTypes = {
   size: PropTypes.oneOf(propSizeType),
+  autoResize: PropTypes.bool,
+};
+
+Textarea.defaultProps = {
+  size: 'medium',
+  autoResize: true,
 };
 
 export default Textarea;
