48b8d03f89c08ebc0186f25ff919ed624acfd5b8
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-08-09 16:22:25 +0900

    [FE] feat : MSW 활용 예시 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,16 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { worker } = require('./mocks/browser');
+  worker.start({
+    serviceWorker: {
+      url: `/mockServiceWorker.js`,
+    },
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(<App />);
--- frontend/src/mocks/handlers/convention.js
+++ frontend/src/mocks/handlers/convention.js
@@ -1,11 +1,15 @@
 /**
  * @author 콤피 <compy.ryu@gmail.com>
- * @comment 예시를 위한 컨벤션 핸들러입니다. MSW 모킹이 어느정도 진행될 시 제거하여주세요.
+ * @comment
+ * 예시를 위한 컨벤션 핸들러입니다. 도메인 별로 모킹 목록을 나누어주세요.
+ * 추후, MSW 모킹이 어느정도 진행될 시 제거하여주세요.
  */
 import { rest } from 'msw';
 
+import { reviewduckAPI } from 'mocks/hosts';
+
 const conventionHandlers = [
-  rest.post('./example', (req, res, ctx) => {
+  rest.post(reviewduckAPI('/example'), (req, res, ctx) => {
     const { userId, password } = req.body;
 
     return res(
