e46400b55c56d256c3fc821182560f67ba525a71
DomMorello <zuzudnf@gmail.com>
2022-08-10 23:10:56 +0900

    [FE] feat : TemplateDetailPage 초안 작성

--- frontend/src/service/@shared/types/template.ts
+++ frontend/src/service/@shared/types/template.ts
@@ -1,13 +1,23 @@
-interface Template {
+export interface Template {
   templateId: number;
   templateTitle: string;
   templateDescription: string;
   creator: {
     nickname: string;
     profileUrl: string;
+    socialId?: string;
+    bio?: string;
   };
   updatedAt: number;
   usedCount: number;
+}
+
+export interface GetTemplateResponse extends Template {
+  questions: {
+    id: number;
+    value: string;
+    description: string;
+  }[];
 }
 
 export type GetTemplatesResponse = Template[];
