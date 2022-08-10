cdae8ed03c36db6eaabb19b757741aef7c4cf710
DomMorello <zuzudnf@gmail.com>
2022-08-10 23:10:09 +0900

    [FE] test : template 단일 조회 mocking

--- frontend/src/mocks/handlers/template.js
+++ frontend/src/mocks/handlers/template.js
@@ -1,11 +1,15 @@
 import { rest } from 'msw';
 
-import { dummyTemplates } from 'mocks/data';
+import { dummyTemplates, dummyTemplate } from 'mocks/data';
 import { reviewduckAPI } from 'mocks/hosts';
 
 const templateHandlers = [
   rest.get(reviewduckAPI('/api/templates'), (req, res, ctx) =>
     res(ctx.status(200), ctx.json(dummyTemplates)),
+  ),
+
+  rest.get(reviewduckAPI('/api/templates/:templateId'), (req, res, ctx) =>
+    res(ctx.status(200), ctx.json(dummyTemplate)),
   ),
 ];
 
