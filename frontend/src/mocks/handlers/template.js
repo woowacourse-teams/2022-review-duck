c9a0a29b3281ba80cbb82b96e0cbb2a8c15c0b19
DomMorello <zuzudnf@gmail.com>
2022-08-10 23:55:37 +0900

    [FE] test : template API 모킹

--- frontend/src/mocks/handlers/template.js
+++ frontend/src/mocks/handlers/template.js
@@ -11,6 +11,14 @@ const templateHandlers = [
   rest.get(reviewduckAPI('/api/templates/:templateId'), (req, res, ctx) =>
     res(ctx.status(200), ctx.json(dummyTemplate)),
   ),
+
+  rest.post(reviewduckAPI('/api/templates/:templateId/review-forms'), (req, res, ctx) =>
+    res(ctx.status(201), ctx.json(dummyFormcode)),
+  ),
+
+  rest.put(reviewduckAPI('/api/templates/:templateId'), (req, res, ctx) => res(ctx.status(204))),
+
+  rest.delete(reviewduckAPI('/api/templates/:templateId'), (req, res, ctx) => res(ctx.status(204))),
 ];
 
 export default templateHandlers;
