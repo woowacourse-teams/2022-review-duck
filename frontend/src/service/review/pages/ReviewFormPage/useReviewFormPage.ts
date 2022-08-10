7d2ed41a7ed5397ccf50a0bcde41eef5d330ae9c
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-08-11 00:54:04 +0900

    [FE] refactor : 회고 폼 생성/수정 페이지 리팩토링
    - API 통신/비즈니스 로직 커스텀훅으로 분리
    - useLocation을 통한 상태 전달 제거

---
+++ frontend/src/service/review/pages/ReviewFormPage/useReviewFormPage.ts
@@ -0,0 +1,51 @@
+import { Question, ReviewForm } from 'service/@shared/types';
+
+import {
+  useCreateReviewForm,
+  useGetReviewForm,
+  useUpdateReviewForm,
+} from 'service/@shared/hooks/queries/review';
+
+function useReviewFormPage(reviewFormCode: string) {
+  const createMutation = useCreateReviewForm();
+  const updateMutation = useUpdateReviewForm();
+
+  const submitMutation = reviewFormCode ? updateMutation : createMutation;
+
+  const getReviewFormQuery = useGetReviewForm(reviewFormCode, {
+    enabled: !!reviewFormCode,
+  });
+
+  const initialReviewForm: ReviewForm = getReviewFormQuery.data || {
+    reviewTitle: '',
+    questions: [
+      {
+        questionValue: '',
+        listKey: 'list-0',
+      },
+    ],
+  };
+
+  const trimQuestions = (questions: Question[]) => {
+    const updatedQuestion = questions.filter((question) => !!question.questionValue?.trim());
+
+    return updatedQuestion.map((question) => {
+      const newQuestion = { ...question };
+
+      delete newQuestion.listKey;
+      return newQuestion;
+    });
+  };
+
+  return {
+    initialReviewForm,
+    isNewReviewForm: !reviewFormCode,
+    isLoadError: getReviewFormQuery.isError,
+    loadError: getReviewFormQuery.error,
+    isSubmitLoading: submitMutation.isLoading,
+    trimQuestions,
+    submitReviewForm: submitMutation.mutate,
+  };
+}
+
+export default useReviewFormPage;
