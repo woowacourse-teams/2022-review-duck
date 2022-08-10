184bd586f860fc1385bbe81c6acc4aac200836dc
DomMorello <zuzudnf@gmail.com>
2022-08-10 10:12:42 +0900

    [FE] refactor : dummy data.js 파일 만들어서 분리

--- frontend/src/mocks/handlers/template.js
+++ frontend/src/mocks/handlers/template.js
@@ -1,57 +1,11 @@
 import { rest } from 'msw';
 
+import { dummyTemplates } from 'mocks/data';
 import { reviewduckAPI } from 'mocks/hosts';
-
-const dummyTemplate = [
-  {
-    templateId: 0,
-    templateTitle: '샬라샬라 템플릿',
-    templateDescription: '이 템플릿을 이런저런이런저런 그런 템플릿입니다.',
-    creator: {
-      nickname: 'dommorello',
-      profileUrl: 'https://avatars.githubusercontent.com/u/51396282?v=4',
-    },
-    updatedAt: 1660049730600,
-    usedCount: 123,
-  },
-  {
-    templateId: 1,
-    templateTitle: '샬라샬라 템플릿',
-    templateDescription: '이 템플릿을 이런저런이런저런 그런 템플릿입니다.',
-    creator: {
-      nickname: 'dommorello',
-      profileUrl: 'https://avatars.githubusercontent.com/u/51396282?v=4',
-    },
-    updatedAt: 1660049730600,
-    usedCount: 123,
-  },
-  {
-    templateId: 2,
-    templateTitle: '샬라샬라 템플릿',
-    templateDescription: '이 템플릿을 이런저런이런저런 그런 템플릿입니다.',
-    creator: {
-      nickname: 'dommorello',
-      profileUrl: 'https://avatars.githubusercontent.com/u/51396282?v=4',
-    },
-    updatedAt: 1660049730600,
-    usedCount: 123,
-  },
-  {
-    templateId: 3,
-    templateTitle: '샬라샬라 템플릿',
-    templateDescription: '이 템플릿을 이런저런이런저런 그런 템플릿입니다.',
-    creator: {
-      nickname: 'dommorello',
-      profileUrl: 'https://avatars.githubusercontent.com/u/51396282?v=4',
-    },
-    updatedAt: 1660049730600,
-    usedCount: 123,
-  },
-];
 
 const templateHandlers = [
   rest.get(reviewduckAPI('/api/templates'), (req, res, ctx) =>
-    res(ctx.status(200), ctx.json(dummyTemplate)),
+    res(ctx.status(200), ctx.json(dummyTemplates)),
   ),
 ];
 
