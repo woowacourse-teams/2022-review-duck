76a80a036d1aecb65a22d4b28f1e5bd73f637848
DomMorello <zuzudnf@gmail.com>
2022-08-12 15:56:42 +0900

    [FE] test : 수정된 API에 따른 mock 업데이츠

--- frontend/src/mocks/handlers/template.js
+++ frontend/src/mocks/handlers/template.js
@@ -4,9 +4,16 @@ import { dummyTemplates, dummyTemplate } from 'mocks/data';
 import { reviewduckAPI } from 'mocks/hosts';
 
 const templateHandlers = [
-  rest.get(reviewduckAPI('/api/templates'), (req, res, ctx) =>
-    res(ctx.status(200), ctx.json(dummyTemplates)),
-  ),
+  rest.get(reviewduckAPI('/api/templates'), (req, res, ctx) => {
+    if (req.url.searchParams.get('filter') === 'trend') {
+      const copyTemplates = [...dummyTemplates.templates];
+      const sortedTemplates = copyTemplates.sort(
+        (first, second) => second.template.usedCount - first.template.usedCount,
+      );
+      return res(ctx.status(200), ctx.json({ templates: sortedTemplates }));
+    }
+    return res(ctx.status(200), ctx.json(dummyTemplates));
+  }),
 
   rest.get(reviewduckAPI('/api/templates/:templateId'), (req, res, ctx) =>
     res(ctx.status(200), ctx.json(dummyTemplate)),
