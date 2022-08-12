0955b432458eb57b02f923e090208a6c57a06219
woong7 <driedseas@gmail.com>
2022-08-12 14:35:01 +0900

    [BE] feat : 무중단 배포 구현

--- backend/reviewduck/src/main/java/com/reviewduck/common/controller/WebRestController.java
+++ backend/reviewduck/src/main/java/com/reviewduck/common/controller/WebRestController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @AllArgsConstructor
@@ -17,7 +16,7 @@ public class WebRestController {
 
     @Operation(summary = "스프링 프로파일을 조회한다.")
     @GetMapping("/profile")
-    public String getProfile(){
+    public String getProfile() {
         return Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
     }
 
