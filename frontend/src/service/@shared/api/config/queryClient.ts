4fd2877813ca695a73374b44c97a9bd14781889e
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-08-13 22:39:35 +0900

    [FE] config : queryClient 기본 설정 수정

--- frontend/src/service/@shared/api/config/queryClient.ts
+++ frontend/src/service/@shared/api/config/queryClient.ts
@@ -3,6 +3,8 @@ import { QueryClient } from 'react-query';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      suspense: true,
+      useErrorBoundary: false,
       refetchOnWindowFocus: false,
       refetchOnReconnect: false,
       retry: false,
