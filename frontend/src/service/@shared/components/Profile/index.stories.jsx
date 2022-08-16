7930ddce5616a9d9f8ade869e6828dddd7c4c3e2
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-08-16 22:56:15 +0900

    [FE] feat : Profile 컴포넌트 추가

---
+++ frontend/src/service/@shared/components/Profile/index.stories.jsx
@@ -0,0 +1,20 @@
+import Profile from '.';
+
+export default {
+  title: '@shared/components/Profile',
+  component: Profile,
+};
+
+const Template = (args) => (
+  <Profile {...args}>
+    <Profile.Image
+      size="medium"
+      theme="rectangle"
+      src="https://avatars.githubusercontent.com/u/94832610?v=4"
+    />
+    <Profile.Nickname>유저 닉네임</Profile.Nickname>
+    <Profile.Description>설명</Profile.Description>
+  </Profile>
+);
+
+export const DefaultNoResult = Template.bind({});
