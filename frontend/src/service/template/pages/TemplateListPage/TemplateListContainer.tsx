08e51e0cd1649b439d917b76ea6e68449f88a3a7
DomMorello <zuzudnf@gmail.com>
2022-08-17 02:17:20 +0900

    [FE] chore : 템플릿 목록 페이지 결과없음 추가

--- frontend/src/service/template/pages/TemplateListPage/TemplateListContainer.tsx
+++ frontend/src/service/template/pages/TemplateListPage/TemplateListContainer.tsx
@@ -1,10 +1,14 @@
 import { GetTemplatesResponse } from 'service/@shared/types/template';
 
+import NoResult from 'service/@shared/components/NoResult';
 import TemplateCard from 'service/template/components/TemplateCard';
 
 import styles from './styles.module.scss';
 
 function TemplateListContainer({ templates }: GetTemplatesResponse) {
+  if (templates.length === 0) {
+    return <NoResult>템플릿이 없습니다.</NoResult>;
+  }
   return (
     <div className={styles.container}>
       {templates.map(({ info, creator }) => (
