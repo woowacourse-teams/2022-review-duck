6dc5cc7f2a5f2f1514ce73f992075b39b5365af0
DomMorello <zuzudnf@gmail.com>
2022-08-17 22:05:54 +0900

    [FE] chore : 템플릭 목록 페이지 템플릿 만들기 버튼 추가

--- frontend/src/service/template/pages/TemplateListPage/index.tsx
+++ frontend/src/service/template/pages/TemplateListPage/index.tsx
@@ -7,6 +7,8 @@ import { TemplateFilterType } from 'service/@shared/types';
 
 import { useGetTemplates } from 'service/@shared/hooks/queries/template/useGet';
 
+import { Button } from 'common/components';
+
 import Icon from 'common/components/Icon';
 
 import LayoutContainer from 'service/@shared/components/LayoutContainer';
@@ -36,30 +38,42 @@ function TemplateListPage() {
   return (
     <LayoutContainer>
       <div>
-        <Link to={`${PAGE_LIST.TEMPLATE_LIST}?filter=${TEMPLATE_TAB.TREND}`}>
-          <button className={styles.button}>
-            <div
-              className={cn(styles.buttonBox, {
-                [styles.focus]: currentTab === TEMPLATE_TAB.TREND,
-              })}
-            >
-              <Icon className={styles.icon} code="local_fire_department" />
-              트랜딩
-            </div>
-          </button>
-        </Link>
-        <Link to={`${PAGE_LIST.TEMPLATE_LIST}?filter=${TEMPLATE_TAB.LATEST}`}>
-          <button className={styles.button}>
-            <div
-              className={cn(styles.buttonBox, {
-                [styles.focus]: currentTab === TEMPLATE_TAB.LATEST,
-              })}
-            >
-              <Icon className={styles.icon} code="playlist_add_check_circle" />
-              최신
-            </div>
-          </button>
-        </Link>
+        <div className={styles.header}>
+          <div>
+            <Link to={`${PAGE_LIST.TEMPLATE_LIST}?filter=${TEMPLATE_TAB.TREND}`}>
+              <button className={styles.button}>
+                <div
+                  className={cn(styles.buttonBox, {
+                    [styles.focus]: currentTab === TEMPLATE_TAB.TREND,
+                  })}
+                >
+                  <Icon className={styles.icon} code="local_fire_department" />
+                  트랜딩
+                </div>
+              </button>
+            </Link>
+            <Link to={`${PAGE_LIST.TEMPLATE_LIST}?filter=${TEMPLATE_TAB.LATEST}`}>
+              <button className={styles.button}>
+                <div
+                  className={cn(styles.buttonBox, {
+                    [styles.focus]: currentTab === TEMPLATE_TAB.LATEST,
+                  })}
+                >
+                  <Icon className={styles.icon} code="playlist_add_check_circle" />
+                  최신
+                </div>
+              </button>
+            </Link>
+          </div>
+          <div className={styles.buttonContainer}>
+            <Link to={PAGE_LIST.TEMPLATE_FORM}>
+              <Button>
+                <Icon code="rate_review" />
+                템플릿 만들기
+              </Button>
+            </Link>
+          </div>
+        </div>
         <TemplateListContainer templates={templates} />
       </div>
     </LayoutContainer>
