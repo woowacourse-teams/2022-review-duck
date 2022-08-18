65904dae348ad05177dd0738001bf57fd23a2366
DomMorello <zuzudnf@gmail.com>
2022-08-18 19:50:19 +0900

    [FE] chore : 메인페이지에 인기 템플릿 목록 출력

--- frontend/src/service/user/pages/MainPage/index.tsx
+++ frontend/src/service/user/pages/MainPage/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 import cn from 'classnames';
 
+import { TemplateFilterType } from 'service/@shared/types';
+
 import useModal from 'common/hooks/useModal';
+import { useGetTemplates } from 'service/@shared/hooks/queries/template';
 
 import { Button, Icon, Text, TransitionDiv } from 'common/components';
 
@@ -9,16 +15,26 @@ import ScrollPanel from 'common/components/ScrollPanel';
 import LayoutContainer from 'service/@shared/components/LayoutContainer';
 import NoResult from 'service/@shared/components/NoResult';
 import QuestionCard from 'service/@shared/components/QuestionCard';
+import TemplateCard from 'service/template/components/TemplateCard';
 
 import styles from './styles.module.scss';
 
-import { MODAL_LIST } from 'service/@shared/constants';
+import { MODAL_LIST, TEMPLATE_TAB } from 'service/@shared/constants';
+import { PAGE_LIST } from 'service/@shared/constants';
 
 function MainPage() {
   const { showModal } = useModal();
   const onClickReviewStart = () => {
     showModal(MODAL_LIST.REVIEW_START);
   };
+  const { data, isError, error } = useGetTemplates(TEMPLATE_TAB.TREND as TemplateFilterType);
+  const { templates } = data || { templates: [] };
+
+  useEffect(() => {
+    if (isError) {
+      alert(error.message);
+    }
+  }, [isError, error]);
 
   return (
     <>
@@ -95,15 +111,24 @@ function MainPage() {
       </LayoutContainer>
 
       <ScrollPanel>
-        <div className={styles.cardTest}></div>
-        <div className={styles.cardTest}></div>
-        <div className={styles.cardTest}></div>
-        <div className={styles.cardTest}></div>
-        <div className={styles.cardTest}></div>
-        <div className={styles.cardTest}></div>
-        <div className={styles.cardTest}></div>
-        <div className={styles.cardTest}></div>
-        <div className={styles.cardTest}></div>
+        {templates.map((template) => (
+          <TemplateCard key={template.info.id} className={styles.card}>
+            <Link to={`${PAGE_LIST.TEMPLATE_DETAIL}/${template.info.id}`}>
+              <TemplateCard.Tag usedCount={template.info.usedCount} />
+              <TemplateCard.Title title={template.info.title} />
+              <TemplateCard.UpdatedAt updatedAt={template.info.updatedAt} />
+              <TemplateCard.Description description={template.info.description} />
+              <TemplateCard.Line />
+            </Link>
+            <Link to={`${PAGE_LIST.USER_PROFILE}/${template.creator.id}`}>
+              <TemplateCard.Profile
+                profileUrl={template.creator.profileUrl}
+                nickname={template.creator.nickname}
+                socialNickname={template.creator.socialNickname || ''}
+              />
+            </Link>
+          </TemplateCard>
+        ))}
       </ScrollPanel>
     </>
   );
