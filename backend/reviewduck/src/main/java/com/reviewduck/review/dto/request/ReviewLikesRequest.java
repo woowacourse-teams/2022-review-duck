a9510b897494f6c1d15547597ac2268a528506ba
ulimy <ulim130@gmail.com>
2022-09-21 13:15:47 +0900

    [BE] style : toString lombok 이용으로 통일

--- backend/reviewduck/src/main/java/com/reviewduck/review/dto/request/ReviewLikesRequest.java
+++ backend/reviewduck/src/main/java/com/reviewduck/review/dto/request/ReviewLikesRequest.java
@@ -6,19 +6,15 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @Getter
+@ToString
 public class ReviewLikesRequest {
 
     @Min(value = 0, message = "좋아요 개수는 0 이상이어야 합니다.")
     private int likes;
 
-    @Override
-    public String toString() {
-        return "ReviewLikesRequest{" +
-            "likes=" + likes +
-            '}';
-    }
 }
