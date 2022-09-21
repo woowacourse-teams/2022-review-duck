abbf1806fde3160d16ee524e424433b3e69003b5
DomMorello <zuzudnf@gmail.com>
2022-09-21 21:39:53 +0900

    [FE] refactor : validateTab 함수 validator/common.ts 로 이동 및 규격 변경

---
+++ frontend/src/service/@shared/validator/common.ts
@@ -0,0 +1,7 @@
+function validateTab(validTabNameArray: string[], currentTab: string) {
+  if (validTabNameArray.findIndex((tab) => tab === currentTab) < 0) {
+    throw new Error('찾을 수 없는 페이지입니다.');
+  }
+}
+
+export { validateTab };
--- frontend/src/service/@shared/validator/index.ts
+++ frontend/src/service/@shared/validator/index.ts
@@ -1,3 +1,4 @@
 export * from './auth';
 export * from './review';
 export * from './user';
+export * from './common';
