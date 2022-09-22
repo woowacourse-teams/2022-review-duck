279c863557e4e510babd14a8afcf0ceccfdf6e55
DomMorello <zuzudnf@gmail.com>
2022-09-23 00:30:51 +0900

    [FE] fix : 무한스크롤 버그 수정

--- frontend/src/common/hooks/useIntersectionObserver.ts
+++ frontend/src/common/hooks/useIntersectionObserver.ts
@@ -18,36 +18,71 @@ function useIntersectionObserver<DataType, RefElementType extends Element>(
   dependArray: unknown[],
 ) {
   const targetRef = useRef<RefElementType>(null);
-  console.log('outer targetRef: ', targetRef);
 
-  const handleIntersect: IntersectionObserverCallback = ([entry]) => {
+  const handleIntersect: IntersectionObserverCallback = ([entry], observer) => {
     if (entry.isIntersecting) {
-      console.log('intersected target: ', entry.target);
+      console.log('intersect: ', entry.target);
+
+      observer.unobserve(entry.target);
       onIntersect();
     }
   };
 
-  const observer = new IntersectionObserver(handleIntersect, option);
-
   useEffect(() => {
-    console.log('useEffect outer targetRef: ', targetRef);
+    console.log('effect: ', targetRef);
 
     if (!targetRef || !targetRef.current) {
       return;
     }
     const target = targetRef.current;
+    const observer = new IntersectionObserver(handleIntersect, option);
 
     observer.observe(target);
+    console.log('observed taret: ', target);
 
-    return () => {
-      if (targetRef && target) {
-        console.log('unobserve targetRef: ', targetRef);
-        observer.unobserve(target);
-      }
-    };
+    return () => observer.disconnect();
   }, dependArray);
 
   return { targetRef };
 }
+// function useIntersectionObserver<DataType, RefElementType extends Element>(
+//   onIntersect: (
+//     options?: FetchNextPageOptions,
+//   ) => Promise<InfiniteQueryObserverResult<InfiniteItem<DataType>>>,
+//   option: ObserverOptionType,
+//   dependArray: unknown[],
+// ) {
+//   const targetRef = useRef<RefElementType>(null);
+//   console.log('outer targetRef: ', targetRef);
+
+//   const handleIntersect: IntersectionObserverCallback = ([entry]) => {
+//     if (entry.isIntersecting) {
+//       console.log('intersected target: ', entry.target);
+//       onIntersect();
+//     }
+//   };
+
+//   const observer = new IntersectionObserver(handleIntersect, option);
+
+//   useEffect(() => {
+//     console.log('useEffect outer targetRef: ', targetRef);
+
+//     if (!targetRef || !targetRef.current) {
+//       return;
+//     }
+//     const target = targetRef.current;
+
+//     observer.observe(target);
+
+//     return () => {
+//       if (targetRef && target) {
+//         console.log('unobserve targetRef: ', targetRef);
+//         observer.unobserve(target);
+//       }
+//     };
+//   }, dependArray);
+
+//   return { targetRef };
+// }
 
 export default useIntersectionObserver;
