7751dc4e77df9a8e926e7e18453fd420321f0db6
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-09-23 07:30:59 +0900

    [FE] config : Cypress 설정 진행

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpackConfig = require('./.webpack/webpack.dev')(null, { options: { mode: 'development' } });
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      config.specPattern = './src/tests/*.spec.ts';
+      config.viewportWidth = 1380;
+      config.viewportHeight = 1080;
+      config.baseUrl = `http://localhost:${webpackConfig.devServer.port}`;
+
+      return config;
+    },
+  },
+});
