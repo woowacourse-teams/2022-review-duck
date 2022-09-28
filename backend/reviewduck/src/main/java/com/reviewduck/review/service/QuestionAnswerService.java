8212e4a04a4beeb31da6af38dfd1fe7caaa2ca01
ulimy <ulim130@gmail.com>
2022-09-28 11:22:08 +0900

    [BE] fix : 회고를 수정한 경우 회고가 삭제되지 않는 버그 수정

---
+++ backend/reviewduck/src/main/java/com/reviewduck/review/service/QuestionAnswerService.java
@@ -0,0 +1,26 @@
+package com.reviewduck.review.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.reviewduck.review.domain.Answer;
+import com.reviewduck.review.domain.QuestionAnswer;
+import com.reviewduck.review.domain.ReviewFormQuestion;
+import com.reviewduck.review.repository.QuestionAnswerRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@AllArgsConstructor
+public class QuestionAnswerService {
+
+    private final QuestionAnswerRepository questionAnswerRepository;
+
+    @Transactional
+    public QuestionAnswer getOrSave(ReviewFormQuestion question, Answer answer) {
+        return questionAnswerRepository.findByAnswerId(answer.getId())
+            .orElse(new QuestionAnswer(question, answer));
+    }
+
+}
