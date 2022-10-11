0d3cbf4f6201f67a718cdc132550a6174a813748
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-10-12 03:18:15 +0900

    [FE] feat : useInfiniteScrollQuery Hook 추가
    - useInfiniteQuery 중복 로직 제거를 위한 hook

--- frontend/src/service/@shared/hooks/queries/useInfiniteScrollQuery.ts
+++ frontend/src/service/@shared/hooks/queries/useInfiniteScrollQuery.ts
@@ -1,23 +1,33 @@
 import { useRef } from 'react';
 
-import { QueryKey, useInfiniteQuery } from '@tanstack/react-query';
+import { QueryKey, useInfiniteQuery, UseInfiniteQueryOptions } from '@tanstack/react-query';
 
 function useInfiniteScrollQuery<
   TQueryFnData = unknown,
+  TError = unknown,
   TData = TQueryFnData,
   TQueryKey extends QueryKey = QueryKey,
->(queryKey: TQueryKey, queryFn: (pageParam: number) => TData, lastListKey: keyof Awaited<TData>) {
+>(
+  queryKey: TQueryKey,
+  queryFn: (pageParam: number) => TData,
+  lastListKey: keyof Awaited<TData>,
+  queryOptions: Omit<
+    UseInfiniteQueryOptions<any, TError, Awaited<TData>, TQueryFnData, TQueryKey>,
+    'queryKey' | 'queryFn'
+  > = {},
+) {
   const pageNumber = useRef(1);
 
   return useInfiniteQuery(
     queryKey,
     async ({ pageParam = 1 }) => {
       pageNumber.current += 1;
-      return queryFn(pageParam);
+      return (await queryFn(pageParam)) as TData;
     },
     {
       getNextPageParam: (data) =>
         data[lastListKey as keyof typeof data] ? undefined : pageNumber.current + 1,
+      ...queryOptions,
     },
   );
 }
