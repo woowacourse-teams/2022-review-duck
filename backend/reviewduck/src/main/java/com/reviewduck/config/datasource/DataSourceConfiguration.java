a3df0c9689d8b88350303d59d27c92700cd3272b
Nam Minjeong <nmj5012@naver.com>
2022-10-18 11:08:04 +0900

    [BE] feat : datasource 이중화 설정 추가

---
+++ backend/reviewduck/src/main/java/com/reviewduck/config/datasource/DataSourceConfiguration.java
@@ -0,0 +1,36 @@
+package com.reviewduck.config.datasource;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    static final String SOURCE = "SOURCE";
+    static final String REPLICA = "REPLICA";
+
+    @Bean
+    @Qualifier(SOURCE)
+    @ConfigurationProperties("spring.datasource.source.hikari")
+    public DataSource sourceDataSource(){
+        return DataSourceBuilder.create()
+            .type(HikariDataSource.class)
+            .build();
+    }
+
+    @Bean
+    @Qualifier(REPLICA)
+    @ConfigurationProperties("spring.datasource.replica.hikari")
+    public DataSource replicaDataSource(){
+        return DataSourceBuilder.create()
+            .type(HikariDataSource.class)
+            .build();
+    }
+}
---
+++ backend/reviewduck/src/main/java/com/reviewduck/config/datasource/DataSourceSelector.java
@@ -0,0 +1,23 @@
+package com.reviewduck.config.datasource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DataSourceSelector {
+
+    private final List<String> dataSources;
+    private Integer counter = 0;
+
+    public DataSourceSelector(String... dataSources) {
+        this.dataSources = Arrays.stream(dataSources)
+            .collect(Collectors.toUnmodifiableList());
+    }
+
+    public String getOne() {
+        if (counter == dataSources.size()) {
+            counter = 0;
+        }
+        return dataSources.get(counter++);
+    }
+}
