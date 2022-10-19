7c3387e5b8cbdfa7a627c40e587ea7ed343bbec4
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2022-10-20 04:03:25 +0900

    [FE] refactor : 페이지 타이틀 변경 로직 Hook 분리

--- frontend/src/service/template/pages/TemplateDetailPage/useTemplateDetailQueries.ts
+++ frontend/src/service/template/pages/TemplateDetailPage/useTemplateDetailQueries.ts
@@ -1,13 +1,26 @@
-import { FILTER } from 'constant';
+import { useEffect } from 'react';
 
+import { FILTER, SERVICE_NAME } from 'constant';
+
+import usePageTitle from 'common/hooks/usePageTitle';
 import { useGetTemplate, useGetTemplates } from 'service/@shared/hooks/queries/template';
 
 function useTemplateDetailQueries(templateId: number) {
   const getTemplateQuery = useGetTemplate(templateId);
+  const pageTitle = usePageTitle(null, SERVICE_NAME);
   const getTrendTemplateQuery = useGetTemplates({ filter: FILTER.TEMPLATE_TAB.TREND });
 
   const isLoading = getTrendTemplateQuery.isLoading || getTemplateQuery.isLoading;
   const isError = getTrendTemplateQuery.isError || getTemplateQuery.isError;
+
+  useEffect(
+    function changePageTitleToTemplateName() {
+      if (!getTemplateQuery.data) return;
+
+      pageTitle.set(getTemplateQuery.data.info.title);
+    },
+    [getTemplateQuery.isSuccess],
+  );
 
   if (isLoading || isError) return false;
 
