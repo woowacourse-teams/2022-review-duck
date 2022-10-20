ea2d36c2bf5718136674a92e552a8b8fb7cb626b
domMorello <zuzudnf@gmail.com>
2022-10-21 00:52:39 +0900

    [FE] chore : 닉네임 오류메세지 수정

--- frontend/src/service/@shared/validator/user.ts
+++ frontend/src/service/@shared/validator/user.ts
@@ -2,7 +2,7 @@ import { REGEX } from 'constant';
 
 function validateNickname(nickname: string) {
   if (!new RegExp(REGEX.NICKNAME, 'g').test(nickname)) {
-    throw new Error('닉네임은 2 ~ 10자 이내로 작성해주세요.');
+    throw new Error('닉네임은 2 ~ 10자 영문,숫자,한글로 작성해주세요.');
   }
 }
 
