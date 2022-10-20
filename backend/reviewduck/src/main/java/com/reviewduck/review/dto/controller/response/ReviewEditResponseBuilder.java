aba6c542277c78dfa94d776297922030b6e5e60c
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-10-20 15:20:48 +0900

    [BE] refactor : 불필요한 주석 제거

--- backend/reviewduck/src/main/java/com/reviewduck/review/dto/controller/response/ReviewEditResponseBuilder.java
+++ backend/reviewduck/src/main/java/com/reviewduck/review/dto/controller/response/ReviewEditResponseBuilder.java
@@ -1,6 +1,5 @@
 package com.reviewduck.review.dto.controller.response;
 
-import java.util.Arrays;
 import java.util.function.Function;
 
 import com.reviewduck.review.domain.Review;
@@ -26,12 +25,5 @@ public enum ReviewEditResponseBuilder {
             return SYNC.responseBuilder.apply(review);
         }
         return NON_SYNC.responseBuilder.apply(review);
-
-        // return Arrays.stream(values())
-        //     .filter(it -> it.isActive() == reviewForm.isActive())
-        //     .findFirst()
-        //     .orElse(NON_SYNC)
-        //     .getResponseBuilder()
-        //     .apply(review);
     }
 }
