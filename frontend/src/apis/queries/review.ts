1becc0b2d949ce6ca2e82c5a50518a202082387d
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2023-03-04 13:05:35 +0900

    [FE] feat : Query Hook 구조 작성

---
+++ frontend/src/apis/queries/review.ts
@@ -0,0 +1,11 @@
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { fetchGetReviewForm, RequestGetReviewForm, ResponseGetReviewForm } from "apis/review";
+
+export function useGetReviewFormQuery(params: RequestGetReviewForm, options: UseQueryOptions<ResponseGetReviewForm> = {}) {
+  
+  return useQuery({
+    queryKey: ["getReviewForm", params],
+    queryFn: () => fetchGetReviewForm(params),
+    ...options,
+  })
+}
