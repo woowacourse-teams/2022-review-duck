c512f148ee89b93a876a3077262f04f2e9a19da2
Ryu Hyun Seung <94832610+compy-ryu@users.noreply.github.com>
2023-04-26 12:55:50 +0900

    [FE] config : ESLint 미사용 설정 제거

--- frontendV2/.storybook/preview.ts
+++ frontendV2/.storybook/preview.ts
@@ -1,8 +1,9 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import '../src/styles/@global.scss';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
